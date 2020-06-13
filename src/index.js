73309b80ed2d1925f95a25de767b8dd7a97e02eb
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-06-13 13:08:44 +0300

    boilerplate done with redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+import store from './store';
+
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 serviceWorker.unregister();
