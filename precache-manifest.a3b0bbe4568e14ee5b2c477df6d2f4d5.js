a65315df53c54f53db5366bd35abb681dd6bd301
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-06-14 23:38:36 +0300

    Updates

---
+++ precache-manifest.a3b0bbe4568e14ee5b2c477df6d2f4d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e187a26294617d066acad19d0f1f7f",
+    "url": "/country-information/index.html"
+  },
+  {
+    "revision": "ea9b82ed389b734a46dd",
+    "url": "/country-information/static/css/main.7ee688b7.chunk.css"
+  },
+  {
+    "revision": "6a76036da3f0ae1b83f3",
+    "url": "/country-information/static/js/2.2690e3f8.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/country-information/static/js/2.2690e3f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9b82ed389b734a46dd",
+    "url": "/country-information/static/js/main.c175c31a.chunk.js"
+  },
+  {
+    "revision": "2bbffc0aa470ba3be057",
+    "url": "/country-information/static/js/runtime-main.310eca74.js"
+  }
+]);
